89cbbe3570bf155eec622c56322c8d963c2380bf
Rodolfo Scarp <rodolfoscarp@gmail.com>
2020-08-16 15:21:57 -0300

    Criadas as rotas e controllers de connections

---
+++ src/controllers/ConnectionsController.ts
@@ -0,0 +1,22 @@
+import { Request, Response, response } from "express";
+import db from "../database/connection";
+
+export default class ConnectionsController {
+  async index(req: Request, res: Response) {
+    const totalConnection = await db('connections').count('* as total');
+
+    const {total} = totalConnection[0];
+
+    return res.json({total})
+  }
+
+  async create(req: Request, res: Response) {
+    const {user_id} = req.body;
+
+    await db('connections').insert({
+        user_id
+    })
+
+    return res.status(201).send();
+  }
+}
